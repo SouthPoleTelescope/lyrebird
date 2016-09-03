00224a0f3a5349b9a723c9b4fda1fbabfe0beb53
ValidName <ValidEmail>
2016-09-02 17:28:58 -0700

    Organizing things

---
+++ python_scripts/aviary.py
@@ -0,0 +1,14 @@
+from spt3g import core, networkstreamer
+import argparse
+
+#need bolo properties faker
+
+#need mapper
+
+#receiver
+
+#sender
+
+#lyrebird config file generator
+
+#kookaburra config file generator
